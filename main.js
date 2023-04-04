0a717d52a7955e93fcd29c6c20a6785e76b65962
skeito <s.keito896@gmail.com>
2023-04-04 21:11:28 +0900

    jsファイルリファクタリング

--- main.js
+++ main.js
@@ -8,121 +8,49 @@ function load_effect() {
 }
 setTimeout(load_effect, 600);
 
-function scroll_down_effect() {
-  let element = document.getElementsByClassName("scroll-down");
-  if (!element) return;
+function scroll_effect(classNameArray) {
+  classNameArray.forEach((className) => {
+    let element = document.getElementsByClassName(className);
+    if (!element) return;
 
-  let scrollY = window.pageYOffset; // スクロール量を取得
-  let windowH = window.innerHeight; // 画面の高さ
-  var showTiming = 200; // 要素を表示するタイミング
-  for (var i = 0; i < element.length; i++) {
-    var elemClientRect = element[i].getBoundingClientRect();
-    var elemY = scrollY + elemClientRect.top;
-    if (scrollY > elemY - windowH + showTiming) {
-      element[i].classList.add("is-show");
+    let scrollY = window.pageYOffset; // スクロール量を取得
+    let windowH = window.innerHeight; // 画面の高さ
+    var showTiming = 200; // 要素を表示するタイミング
+    for (var i = 0; i < element.length; i++) {
+      var elemClientRect = element[i].getBoundingClientRect();
+      var elemY = scrollY + elemClientRect.top;
+      if (scrollY > elemY - windowH + showTiming) {
+        element[i].classList.add("is-show");
+      }
     }
-  }
+  });
 }
-window.addEventListener("scroll", scroll_down_effect);
-
-function scroll_fast_down_effect() {
-  let element = document.getElementsByClassName("scroll-fast-down");
-  if (!element) return;
-
-  let scrollY = window.pageYOffset; // スクロール量を取得
-  let windowH = window.innerHeight; // 画面の高さ
-  var showTiming = 200; // 要素を表示するタイミング
-  for (var i = 0; i < element.length; i++) {
-    var elemClientRect = element[i].getBoundingClientRect();
-    var elemY = scrollY + elemClientRect.top;
-    if (scrollY > elemY - windowH + showTiming) {
-      element[i].classList.add("is-show");
-    }
+window.addEventListener(
+  "scroll",
+  () => {
+    scroll_effect([
+      "scroll-down",
+      "scroll-fast-down",
+      "scroll-delay-down",
+      "scroll-left",
+      "scroll-right",
+    ]);
   }
-}
-window.addEventListener("scroll", scroll_fast_down_effect);
+);
 
-function scroll_delay_down_effect() {
-  let element = document.getElementsByClassName("scroll-delay-down");
-  if (!element) return;
-
-  let scrollY = window.pageYOffset; // スクロール量を取得
-  let windowH = window.innerHeight; // 画面の高さ
-  var showTiming = 200; // 要素を表示するタイミング
-  for (var i = 0; i < element.length; i++) {
-    var elemClientRect = element[i].getBoundingClientRect();
-    var elemY = scrollY + elemClientRect.top;
-    if (scrollY > elemY - windowH + showTiming) {
-      element[i].classList.add("is-show");
-    }
-  }
-}
-window.addEventListener("scroll", scroll_delay_down_effect);
-
-function scroll_right_effect() {
-  let element = document.getElementsByClassName("scroll-right");
-  if (!element) return;
-
-  let scrollY = window.pageYOffset; // スクロール量を取得
-  let windowH = window.innerHeight; // 画面の高さ
-  var showTiming = 200; // 要素を表示するタイミング
-  for (var i = 0; i < element.length; i++) {
-    var elemClientRect = element[i].getBoundingClientRect();
-    var elemY = scrollY + elemClientRect.top;
-    if (scrollY > elemY - windowH + showTiming) {
-      element[i].classList.add("is-show");
-    }
-  }
-}
-window.addEventListener("scroll", scroll_right_effect);
-
-function scroll_right_effect() {
-  let element = document.getElementsByClassName("scroll-right");
-  if (!element) return;
-
-  let scrollY = window.pageYOffset; // スクロール量を取得
-  let windowH = window.innerHeight; // 画面の高さ
-  var showTiming = 200; // 要素を表示するタイミング
-  for (var i = 0; i < element.length; i++) {
-    var elemClientRect = element[i].getBoundingClientRect();
-    var elemY = scrollY + elemClientRect.top;
-    if (scrollY > elemY - windowH + showTiming) {
-      element[i].classList.add("is-show");
-    }
-  }
-}
-window.addEventListener("scroll", scroll_right_effect);
-
-function scroll_left_effect() {
-  let element = document.getElementsByClassName("scroll-left");
-  if (!element) return;
-
-  let scrollY = window.pageYOffset; // スクロール量を取得
-  let windowH = window.innerHeight; // 画面の高さ
-  var showTiming = 200; // 要素を表示するタイミング
-  for (var i = 0; i < element.length; i++) {
-    var elemClientRect = element[i].getBoundingClientRect();
-    var elemY = scrollY + elemClientRect.top;
-    if (scrollY > elemY - windowH + showTiming) {
-      element[i].classList.add("is-show");
-    }
-  }
-}
-window.addEventListener("scroll", scroll_left_effect);
-
-const documentBody = document.querySelector('body');
+const documentBody = document.querySelector("body");
 // body直下にオーバーレイの要素を作成
 var newElement = document.createElement("div");
-newElement.setAttribute("class","progress-bar");
+newElement.setAttribute("class", "progress-bar");
 documentBody.prepend(newElement);
-(function progressBar(){
-  const bar = document.querySelector('.progress-bar');
-  window.addEventListener('scroll', function updateBar(){
+(function progressBar() {
+  const bar = document.querySelector(".progress-bar");
+  window.addEventListener("scroll", function updateBar() {
     const windowScroll = window.pageYOffset;
     const docHeight = document.body.clientHeight;
     const windowHeight = window.innerHeight;
-   
-    const scrollUnit = (windowScroll / (docHeight - windowHeight) * 100);
+
+    const scrollUnit = (windowScroll / (docHeight - windowHeight)) * 100;
     bar.style.height = scrollUnit + "%";
-  })
-}());
+  });
+})();
